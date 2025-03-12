1bac29fb3d55f9441f6cf676e173ade722bab132
midhundominic <111724929+midhundominic@users.noreply.github.com>
2025-03-12 20:36:11 +0530

    Auth

--- backend/Routes/biometricRoutes.js
+++ backend/Routes/biometricRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require('../middleware/auth');
 const biometricController = require("../controllers/biometricController")
+//Auth Middlware removed from biometric
 
-
-router.post('/register', authMiddleware,biometricController.generateRegistrationOptions);
-router.post('/verify-registration', authMiddleware,biometricController.verifyRegistration);
+router.post('/register',biometricController.generateRegistrationOptions);
+router.post('/verify-registration',biometricController.verifyRegistration);
 router.post('/authenticate-biometric',biometricController.generateAuthenticationOptions);
 router.post('/verify-authentication',biometricController.verifyAuthentication);
 
